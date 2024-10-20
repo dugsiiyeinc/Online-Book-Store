df706c15182241f909f176bf2a2498532623c503
Ac <abdullahiac844@gmail.com>
2024-10-20 16:21:33 +0300

    feat: Set up hero section structure with dynamic functionality ; inssue no (#1).
    - Add <section> tag with ID 'hero'.
    - Implement JavaScript for dynamic behavior.
    - Apply basic styling for initial layout   .   🚀

--- home/custom.js
+++ home/custom.js
@@ -1,18 +1,48 @@
-//? Select the elements for menu open and close actions
-const openMenu = document.querySelector('#open-menu'); //? Menu icon
-const closeMenu = document.querySelector('#close-menu'); //? Close icon
-const mobileNav = document.querySelector('.mobileScreens'); //? Mobile navigation
+//? Mobile menu
+const openMenu = document.querySelector('#open-menu');
+const closeMenu = document.querySelector('#close-menu');
+const mobileNav = document.querySelector('.mobileScreens');
 
-//? Function to open the menu
+
 openMenu.addEventListener('click', () => {
     mobileNav.classList.add('active'); //? Show the mobile menu
     openMenu.style.display = 'none'; //? Hide the menu icon
     closeMenu.style.display = 'block'; //? Show the close icon
 });
 
-// Function to close the menu
 closeMenu.addEventListener('click', () => {
     mobileNav.classList.remove('active'); //? Hide the mobile menu
     closeMenu.style.display = 'none'; //? Hide the close icon
     openMenu.style.display = 'block'; //? Show the menu icon
 });
+
+//? hero section , background images adding as dynamic .
+const heroBg = [
+    'url("/images/bgHero-3.png")',
+    'url("/images/bgHero-1.png")',
+    'url("/images/bgHero-3.png")',
+    'url("/images/bgHero-2.png")',
+    'url("/images/bgHero-3.png")',
+]
+//* test
+// console.log(heroBg)
+
+heroBg.forEach((image) => {
+    //* test
+    // console.log("img one: ", img)
+    const img = new Image()
+    img.src = image
+})
+
+let indexId = 0 ;
+
+function ChangeHeroBg () {
+    setTimeout(() => {
+        document.querySelector("#hero").style.background = heroBg[indexId]
+    }, 1000 * 5);
+    indexId = (indexId + 1) % heroBg.length
+    console.log(heroBg[indexId])
+}
+setInterval(ChangeHeroBg, 4000);
+
+ChangeHeroBg()
