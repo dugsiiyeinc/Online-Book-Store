044086b2194e50496a6db905fd0d412b9dcd9c2e
Ac <abdullahiac844@gmail.com>
2024-10-22 09:27:58 +0300

    feat: Finish hero section with responsive design . Inssue Number (#1)
    - Add the complete HTML structure for the hero section.
    - Apply CSS styles to make it look good  .
    - I made sure it works well on all screen sizes and is responsive.

--- home/custom.js
+++ home/custom.js
@@ -2,47 +2,18 @@
 const openMenu = document.querySelector('#open-menu');
 const closeMenu = document.querySelector('#close-menu');
 const mobileNav = document.querySelector('.mobileScreens');
-
+const heroSection = document.querySelector('#hero');
 
 openMenu.addEventListener('click', () => {
-    mobileNav.classList.add('active'); //? Show the mobile menu
-    openMenu.style.display = 'none'; //? Hide the menu icon
-    closeMenu.style.display = 'block'; //? Show the close icon
+    mobileNav.classList.add('active');
+    openMenu.style.display = 'none';
+    closeMenu.style.display = 'block';
 });
 
 closeMenu.addEventListener('click', () => {
-    mobileNav.classList.remove('active'); //? Hide the mobile menu
-    closeMenu.style.display = 'none'; //? Hide the close icon
-    openMenu.style.display = 'block'; //? Show the menu icon
+    mobileNav.classList.remove('active');
+    closeMenu.style.display = 'none';
+    openMenu.style.display = 'block';
 });
 
-//? hero section , background images adding as dynamic .
-const heroBg = [
-    'url("/images/bgHero-3.png")',
-    'url("/images/bgHero-1.png")',
-    'url("/images/bgHero-3.png")',
-    'url("/images/bgHero-2.png")',
-    'url("/images/bgHero-3.png")',
-]
-//* test
-// console.log(heroBg)
 
-heroBg.forEach((image) => {
-    //* test
-    // console.log("img one: ", img)
-    const img = new Image()
-    img.src = image
-})
-
-let indexId = 0 ;
-
-function ChangeHeroBg () {
-    setTimeout(() => {
-        document.querySelector("#hero").style.background = heroBg[indexId]
-    }, 1000 * 5);
-    indexId = (indexId + 1) % heroBg.length
-    console.log(heroBg[indexId])
-}
-setInterval(ChangeHeroBg, 4000);
-
-ChangeHeroBg()
