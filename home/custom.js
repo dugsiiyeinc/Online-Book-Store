48868632da90c4523ecb937f8c0b3c7e778645d3
Ac <abdullahiac844@gmail.com>
2024-10-22 21:44:20 +0300

    feat: Add dynamic content image to hero section. Inssue Number: (#1).
    - Implement JavaScript to display a dynamic content image on the right side of the hero section.
    - Ensure the image updates correctly.

--- home/custom.js
+++ home/custom.js
@@ -1,4 +1,5 @@
-//? Mobile menu
+/* === Mobile menu === */
+
 const openMenu = document.querySelector('#open-menu');
 const closeMenu = document.querySelector('#close-menu');
 const mobileNav = document.querySelector('.mobileScreens');
@@ -16,6 +17,7 @@ closeMenu.addEventListener('click', () => {
     openMenu.style.display = 'block';
 });
 
+/* === blur background === */
 
 openMenu.addEventListener('click', () => {
     heroSection.classList.add("blur");
@@ -27,3 +29,33 @@ closeMenu.addEventListener('click', () => {
 });
 
 
+/* === content hero image === */
+
+const contentHeroImg = [
+    "/images/image-01.png",
+    "/images/image-02.png",
+    "/images/image-003.png",
+    "/images/image-04.png",
+
+];
+
+let index = 0;
+const imgContentHero = document.querySelector("#contentHero-img");
+
+function addContentHeroImg() {
+
+    imgContentHero.innerHTML = "";
+
+    const img = document.createElement("img");
+    img.src = contentHeroImg[index];
+    img.style.width = "100%";
+    img.style.height = "500px";
+    img.style.objectFit = "cover";
+
+    imgContentHero.appendChild(img);
+
+    index = (index + 1) % contentHeroImg.length;
+}
+
+setInterval(addContentHeroImg, 2000);
+addContentHeroImg(); 
