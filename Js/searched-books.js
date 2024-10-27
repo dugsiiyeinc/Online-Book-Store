288c465a6472edcbfbc7c2a0024e3523f4c674c5
Nasrah-muse <nazronafta143@gmail.com>
2024-10-27 17:59:11 +0300

    feat: add header and section for displaying search results

--- Js/searched-books.js
+++ Js/searched-books.js
@@ -3,3 +3,12 @@ function getQueryParam(param) {
     const urlParams = new URLSearchParams(window.location.search);
     return urlParams.get(param);
 }
+async function displaySearchedBooks() {
+    const query = getQueryParam("query")?.toLowerCase() || ""; 
+    const bookDisplay = document.getElementById("bookDisplay");
+    const searchQueryDisplay = document.getElementById("searchQueryDisplay");  
+    const searchQuery = document.getElementById("searchQuery");
+
+     searchQueryDisplay.value = query; 
+    searchQuery.textContent = `Showing results for "${query}"`;  
+}
