2cac590a63e9a3de9185ef1c9b662f9a6341de92
Nasrah-muse <nazronafta143@gmail.com>
2024-11-07 21:29:16 +0300

    feat(main.js): add mobile menu, cart modal, and newsletter subscription

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,42 @@
+/* === Mobile menu === */
+const openMenu = document.querySelector('#open-menu');
+const closeMenu = document.querySelector('#close-menu');
+const mobileNav = document.querySelector('.mobileScreens');
+const heroSection = document.querySelector('#hero');
+
+openMenu.addEventListener('click', () => {
+    mobileNav.classList.add('active');
+    openMenu.style.display = 'none';
+    closeMenu.style.display = 'block';
+});
+
+closeMenu.addEventListener('click', () => {
+    mobileNav.classList.remove('active');
+    closeMenu.style.display = 'none';
+    openMenu.style.display = 'block';
+});
+
+/* === blur background === */
+
+openMenu.addEventListener('click', () => {
+    heroSection.classList.add("blur");
+    console.log("cool")
+});
+closeMenu.addEventListener('click', () => {
+    heroSection.classList.remove("blur");
+    console.log("cool")
+});
+
+
+/* === content hero image === */
+
+const contentHeroImg = [
+    "/images/image-01.png",
+    "/images/image-02.png",
+    "/images/image-003.png",
+    "/images/image-04.png",
+
+];
 
 let index = 0;
 const imgContentHero = document.querySelector("#contentHero-img");
