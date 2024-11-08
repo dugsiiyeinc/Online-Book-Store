3f1176d4554846fb76c2d3439ae221426625632d
Nasrah-muse <nazronafta143@gmail.com>
2024-11-08 16:34:19 +0300

    feat(cart): show message for empty cart

--- Js/cart.js
+++ Js/cart.js
@@ -2,4 +2,8 @@ let cartItems = JSON.parse(localStorage.getItem("cartItems")) || [];
 function displayCart() {
     const cartContainer = document.getElementById("cartContainer");
     cartContainer.innerHTML = '';
+    if (cartItems.length === 0) {
+        cartContainer.innerHTML = '<p>Your cart is empty.</p>';
+        return;
+    }
 }
