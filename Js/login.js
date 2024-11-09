6a0d092312e506101ed9019dafacabacd90c4dab
Nasrah-muse <nazronafta143@gmail.com>
2024-11-09 22:19:06 +0300

    feat: handle login form submission

--- Js/login.js
+++ Js/login.js
@@ -1,2 +1,9 @@
      const loginForm = document.querySelector(".login-form");
-     
+     loginForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        // email and password
+        const email = loginForm.querySelector("input[placeholder='Email']").value.trim();
+        const password = loginForm.querySelector("input[placeholder='Password']").value;
+     });
+
